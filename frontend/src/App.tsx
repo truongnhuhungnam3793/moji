1289bacf17d6b6636469ab3a75990b22c3871ce3
HUNGNAMHOMEPC\truon <truongnhuhungnam@gmail.com>
2025-11-30 22:37:07 +0700

    feat: implement basic routing with authentication protection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router"
-import ChatApp from "./pages/ChatApp"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import { Toaster } from "sonner"
+import ProtectedRoute from "@/components/auth/ProtectedRoute";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Toaster } from "sonner";
+import ChatApp from "./pages/ChatApp";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -12,11 +13,13 @@ function App() {
         <Routes>
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/" element={<ChatApp />} />
+          <Route path="/" element={<ProtectedRoute />}>
+            <Route path="/" element={<ChatApp />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/auth/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import { useAuthStore } from "@/stores/useAuthStore";
+import { Navigate, Outlet } from "react-router";
+
+const ProtectedRoute = () => {
+  const { accessToken, user, loading } = useAuthStore();
+
+  if (!accessToken) {
+    return <Navigate to="/signin" replace />;
+  }
+
+  return <Outlet />;
+};
+export default ProtectedRoute;
