101bedc55496fa48da5213fd5c19983f0c8f927f
HUNGNAMHOMEPC\truon <truongnhuhungnam@gmail.com>
2025-11-30 14:59:32 +0700

    feat: Add sign-up page and form with new frontend dependencies and Prettier configuration.

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,12 +1,18 @@
-import { SignupForm } from "@/components/signup-form"
+import { SignupForm } from "@/components/signup-form";
 
 const SignUp = () => {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div
+      style={{
+        background:
+          "radial-gradient(125% 125% at 50% 10%, #fff 40%, #7c3aed 100%)",
+      }}
+      className="bg-muted absolute inset-0 z-0 flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10"
+    >
       <div className="w-full max-w-sm md:max-w-4xl">
         <SignupForm />
       </div>
     </div>
-  )
-}
-export default SignUp
+  );
+};
+export default SignUp;
